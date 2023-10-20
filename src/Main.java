3ffe1496a87696939a4d57cf96cb531dc8c80384
Cloudy34 <Jenykumar.Patel@strathmore.edu>
2023-10-20 16:03:45 +0300

    Queue Stats

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,117 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import SimNMod.QueueStatistics;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Main {
+
+    private static final int ARRIVAL = 0;
+    private static final int DEPARTURE = 1;
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Stepp 1  - Create scanner so user inputs No of Customers
+        Scanner scanner = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.print("Enter the number of customers: ");
+        int numCustomers = scanner.nextInt();
+
+        double[] IAT = new double[numCustomers];
+        double[] ST = new double[numCustomers];
+
+        System.out.println("Enter inter-arrival times (IAT) and service times (ST) for each customer:");
+        // Step 2 - For all Customers Users have to input IATs and STs, thats why for loop is created
+        for (int i = 0; i < numCustomers; i++) {
+            System.out.print("IAT[" + i + "]: ");
+            IAT[i] = scanner.nextDouble();
+
+            System.out.print("ST[" + i + "]: ");
+            ST[i] = scanner.nextDouble();
+        }
+
+        //Step 3 -  Run the simulation with IAT and ST that user provides
+        QueueStatistics predictedQueueStats = predictQueueStatistics(IAT, ST);
+
+        //Step 4 - Displaying the predicted queue stats
+        System.out.println("Predicted Queue Statistics:");
+        System.out.println("Number of Customers in Queue: " + predictedQueueStats.getNumCustomersInQueue());
+        System.out.println("Number of Customers in System: " + predictedQueueStats.getNumCustomersInSystem());
+        System.out.println("Total Customers Served: " + predictedQueueStats.getTotalCustomersServed());
+        System.out.println("Average Queue Wait Time: " + predictedQueueStats.getAverageQueueWaitTime());
+
+        scanner.close();
+    }
+
+    private static QueueStatistics predictQueueStatistics(double[] IAT, double[] ST) {
+        // Step 1 Initializing Simulation variables
+        int numCustomersInQueue = 0;
+        int numCustomersInSystem = 0;
+        int totalCustomersServed = 0;
+        double totalQueueWaitTime = 0.0;
+        double simulationTime = 0.0;
+
+        // Step 2 - Using linked listing for Event queue
+        Queue<Event> eventQueue = new LinkedList<>();
+
+        // Step 3 - Initialize with the first arrival event
+        eventQueue.add(new Event(ARRIVAL, simulationTime + IAT[0]));
+
+        while (!eventQueue.isEmpty()) {
+            Event currentEvent = eventQueue.poll();
+            simulationTime = currentEvent.getTime();
+
+            if (currentEvent.getType() == ARRIVAL) {
+                numCustomersInSystem++;
+
+                if (numCustomersInQueue == 0) {
+                    eventQueue.add(new Event(DEPARTURE, simulationTime + ST[totalCustomersServed]));
+                } else {
+                    numCustomersInQueue++;
+                }
+
+                if (totalCustomersServed < IAT.length - 1) {
+                    eventQueue.add(new Event(ARRIVAL, simulationTime + IAT[totalCustomersServed + 1]));
+                }
+
+            } else if (currentEvent.getType() == DEPARTURE) {
+                numCustomersInQueue--;
+                totalCustomersServed++;
+                totalQueueWaitTime += simulationTime - IAT[totalCustomersServed];
+
+                if (numCustomersInQueue > 0) {
+                    eventQueue.add(new Event(DEPARTURE, simulationTime + ST[totalCustomersServed]));
+                }
+            }
+        }
+
+        double averageQueueWaitTime = totalQueueWaitTime / totalCustomersServed;
+
+        // Step 4 - Creating and returning QueueStatistics objects
+        QueueStatistics queueStatistics = new QueueStatistics();
+        queueStatistics.setNumCustomersInQueue(numCustomersInQueue);
+        queueStatistics.setNumCustomersInSystem(numCustomersInSystem);
+        queueStatistics.setTotalCustomersServed(totalCustomersServed);
+        queueStatistics.setTotalQueueWaitTime(averageQueueWaitTime);
+
+        return queueStatistics;
+    }
+
+    private static class Event {
+        private int type;
+        private double time;
+
+        public Event(int type, double time) {
+            this.type = type;
+            this.time = time;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public double getTime() {
+            return time;
         }
     }
 }
---
+++ src/SimNMod/QueueStatistics.java
@@ -0,0 +1,54 @@
+package SimNMod;
+
+public class QueueStatistics {
+    private int numCustomersInQueue;
+    private int numCustomersInSystem;
+    private int totalCustomersServed;
+    private double averageQueueWaitTime;
+
+
+    public QueueStatistics() {
+        // Default constructor
+    }
+
+    public QueueStatistics(int numCustomersInQueue, int numCustomersInSystem, int totalCustomersServed, double averageQueueWaitTime) {
+        this.numCustomersInQueue = numCustomersInQueue;
+        this.numCustomersInSystem = numCustomersInSystem;
+        this.totalCustomersServed = totalCustomersServed;
+        this.averageQueueWaitTime = averageQueueWaitTime;
+    }
+
+    // Getters
+    public int getNumCustomersInQueue() {
+        return numCustomersInQueue;
+    }
+
+    public int getNumCustomersInSystem() {
+        return numCustomersInSystem;
+    }
+
+    public int getTotalCustomersServed() {
+        return totalCustomersServed;
+    }
+
+    public double getAverageQueueWaitTime() {
+        return averageQueueWaitTime;
+    }
+
+    // Setters
+    public void setNumCustomersInQueue(int numCustomersInQueue) {
+        this.numCustomersInQueue = numCustomersInQueue;
+    }
+
+    public void setNumCustomersInSystem(int numCustomersInSystem) {
+        this.numCustomersInSystem = numCustomersInSystem;
+    }
+
+    public void setTotalCustomersServed(int totalCustomersServed) {
+        this.totalCustomersServed = totalCustomersServed;
+    }
+
+    public void setTotalQueueWaitTime(double averageQueueWaitTime) {
+        this.averageQueueWaitTime = averageQueueWaitTime;
+    }
+}
